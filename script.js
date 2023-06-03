ed8b1805bf6a5b5434ddefa1d20019fd6a67ca58
shiva-saini <shivasainisiyana2211@gmail.com>
2023-06-03 20:25:15 +0530

    ms-clone created

---
+++ script.js
@@ -0,0 +1,19 @@
+// var ans = prompt("enter your name")
+
+// if(ans != null){
+//     alert("your ans is " + ans);
+// }else{
+//     alert("you cancelled the process");
+// }
+
+
+let y  = 10;
+
+function myFunc(){
+    let y = 30;
+    
+    console.log(y);
+}
+
+myFunc();
+console.log(y);
